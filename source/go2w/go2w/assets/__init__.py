f1121c7d1dccf69f788be3ae5cb11a86e85783f4
suyourice <suyourice@gmail.com>
2026-04-07 17:38:27 +0200

    feat: add Go2-W ArticulationCfg and gym environment registration
    - add GO2W_CFG with 16-DOF hybrid actuator setup:
      - 4 wheel joints (velocity control, stiffness=0, damping=0.5)
      - 12 leg joints (position control, stiffness=40, damping=2)
    - set init_state for upright standing pose (z=0.50 m, thigh=0.4 rad, calf=-0.84 rad) based on kinematic height calculation
    - register Flat-Go2w-v0 (train) and Flat-Go2w-Play-v0 (eval)
    - configure PPO: 96 steps/env, 1500 iter, [256,128,128] network

---
+++ source/go2w/go2w/assets/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022-2026, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Robot asset configurations for the go2w package."""
+
+from .go2w import GO2W_CFG
